b98f17bdb943f7e0b9085b0a5fa9b4e60d1586fe
Terkadur <47321551+Terkadur@users.noreply.github.com>
2020-01-06 21:06:23 +0200

    Update cyclic_newer_.js

--- cyclic_newer_.js
+++ cyclic_newer_.js
@@ -1,1 +1,7 @@
-
+schedule_tues = [
+  [timeToSec(9, 0), timeToSec(10, 0)],
+  [timeToSec(10, 5), timeToSec(11, 0)],
+  [timeToSec(11, 30), timeToSec(12, 30)],
+  [timeToSec(12, 35), timeToSec(13, 30)],
+  [timeToSec(14, 15), timeToSec(15, 0)]
+                ];
